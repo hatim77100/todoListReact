12b54e79af3d6485d7fcc260561dcde3027c7f1e
hatim77100 <najoua-hatim@hotmail.fr>
2022-12-12 23:06:12 +0100

    add editTodo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import AddToDo from './components/AddToDo';
-import TodoList from './components/TodoList';
-import {useState} from 'react';
+import "./App.css";
+import AddToDo from "./components/AddToDo";
+import TodoList from "./components/TodoList";
+import { useState } from "react";
 
 function App() {
-
   const [todoList, setTodoList] = useState([]);
 
   function addTodo(content) {
@@ -12,36 +11,68 @@ function App() {
       id: crypto.randomUUID(),
       content,
       done: false,
-      edit: false
-    }
-    setTodoList([...todoList, todo])
+      edit: false,
+    };
+    setTodoList([...todoList, todo]);
   }
 
   function deleteTodo(id) {
-    setTodoList(todoList.filter(todo => todo.id !== id))
+    setTodoList(todoList.filter((todo) => todo.id !== id));
   }
 
   function toggleTodo(id) {
-    setTodoList(todoList.map( todo => todo.id === id ? ({
-      ...todo,
-      done: !todo.done
-    }) : todo));
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              done: !todo.done,
+            }
+          : todo
+      )
+    );
   }
 
   function toggleTodoEdit(id) {
-    setTodoList(todoList.map( todo => todo.id === id ? ({
-      ...todo,
-      edit: !todo.edit,
-    }) : todo));
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              edit: !todo.edit,
+            }
+          : todo
+      )
+    );
+  }
+
+  function editTodo(id, content) {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              edit: false,
+              content,
+            }
+          : todo
+      )
+    );
   }
 
   return (
     <div className="d-flex flex-row justify-center items-center p-20">
-     <div className="card p-20">
-      <h1 className="mb-20">Todo list</h1>
-      <AddToDo addTodo={addTodo}/>
-      <TodoList todoList={todoList} deleteTodo={deleteTodo} toggleTodo= {toggleTodo} toggleTodoEdit={toggleTodoEdit}/>
-     </div>
+      <div className="card p-20">
+        <h1 className="mb-20">Todo list</h1>
+        <AddToDo addTodo={addTodo} />
+        <TodoList
+          todoList={todoList}
+          deleteTodo={deleteTodo}
+          toggleTodo={toggleTodo}
+          toggleTodoEdit={toggleTodoEdit}
+          editTodo={editTodo}
+        />
+      </div>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,33 @@
 import TodoItem from "./TodoItem";
 import EditTodo from "./EditTodo";
 
-function TodoList({ todoList, deleteTodo, toggleTodo, toggleTodoEdit}) {
+function TodoList({
+  todoList,
+  deleteTodo,
+  toggleTodo,
+  toggleTodoEdit,
+  editTodo,
+}) {
   return todoList.length ? (
     <ul>
-      {todoList.map((todo) => todo.edit ? (<EditTodo key={todo.id} todo={todo}/>):(
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          editTodo={() => toggleTodoEdit(todo.id)}
-          deleteTodo={() => deleteTodo(todo.id)}
-          toggleTodo ={() => toggleTodo(todo.id)}
-        />
-      ))}
+      {todoList.map((todo) =>
+        todo.edit ? (
+          <EditTodo
+            key={todo.id}
+            todo={todo}
+            cancelEditTodo ={()=>toggleTodoEdit(todo.id)}
+            editTodo={(content) => editTodo(todo.id, content)}
+          />
+        ) : (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            editTodo={() => toggleTodoEdit(todo.id)}
+            deleteTodo={() => deleteTodo(todo.id)}
+            toggleTodo={() => toggleTodo(todo.id)}
+          />
+        )
+      )}
     </ul>
   ) : (
     <p>Aucune todo pour le moment</p>
