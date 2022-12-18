e4e7fa641e18d74fab81c80f6be729d3380eeb32
hatim77100 <najoua-hatim@hotmail.fr>
2022-12-18 22:13:43 +0100

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,107 +1,88 @@
 import "./App.css";
 import AddToDo from "./components/AddToDo";
 import TodoList from "./components/TodoList";
-import { useState } from "react";
+import { useReducer } from "react";
 import themeContext from "./context/theme";
+import todoReducer from "./reducers/todoReducer";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [state, dispatch] = useReducer(todoReducer, {
+    theme: "primary",
+    todoList: [],
+  });
 
   function addTodo(content) {
-    const todo = {
-      id: crypto.randomUUID(),
+    dispatch({
+      type: 'ADD_TODO',
       content,
-      done: false,
-      edit: false,
-      selected: false,
-    };
-    setTodoList([...todoList, todo]);
+    })
   }
 
   function deleteTodo(id) {
-    setTodoList(todoList.filter((todo) => todo.id !== id));
+    dispatch({
+      type: 'DELETE_TODO',
+      id
+    })
   }
 
   function toggleTodo(id) {
-    setTodoList(
-      todoList.map((todo) =>
-        todo.id === id
-          ? {
-              ...todo,
-              done: !todo.done,
-            }
-          : todo
-      )
-    );
+    dispatch({
+      type: 'TOGGLE_TODO',
+      id
+    })
   }
 
   function toggleTodoEdit(id) {
-    setTodoList(
-      todoList.map((todo) =>
-        todo.id === id
-          ? {
-              ...todo,
-              edit: !todo.edit,
-            }
-          : todo
-      )
-    );
+    dispatch({
+      type: 'TOGGLE_EDIT_TODO',
+      id
+    })
   }
 
   function editTodo(id, content) {
-    setTodoList(
-      todoList.map((todo) =>
-        todo.id === id
-          ? {
-              ...todo,
-              edit: false,
-              content,
-            }
-          : todo
-      )
-    );
+    dispatch({
+      type: 'EDIT_TODO',
+      id,
+      content
+    })
   }
 
   function selectTodo(id) {
-    setTodoList(
-      todoList.map((todo) =>
-        todo.id === id
-          ? {
-              ...todo,
-              selected: true,
-            }
-          : { ...todo, selected: false }
-      )
-    );
+    dispatch({
+      type: 'SELECT_TODO',
+      id
+    })
   }
 
-  const[theme, setTheme] = useState('primary');
   function handleChange(e) {
-    setTheme(e.target.value)
+    dispatch({
+      type: 'SET_THEME',
+      theme: e.target.value,
+    })
   }
 
   return (
-    <themeContext.Provider value={theme}>
-    <div className="d-flex flex-row justify-center items-center p-20">
-      <div className="card p-20">
-        <h1 className="mb-20 d-flex flex-row justify-center items-center">
-          <span className="flex-fill">Todo list</span> 
-          <select onChange={handleChange} value={theme}>
-            <option value="primary">Orange</option>
-            <option value="secondary">Bleu</option>
-          </select>
+    <themeContext.Provider value={state.theme}>
+      <div className="d-flex flex-row justify-center items-center p-20">
+        <div className="card p-20">
+          <h1 className="mb-20 d-flex flex-row justify-center items-center">
+            <span className="flex-fill">Todo list</span>
+            <select onChange={handleChange} value={state.theme}>
+              <option value="primary">Orange</option>
+              <option value="secondary">Bleu</option>
+            </select>
           </h1>
-        <AddToDo addTodo={addTodo} />
-        <TodoList
-          todoList={todoList}
-          deleteTodo={deleteTodo}
-          toggleTodo={toggleTodo}
-          toggleTodoEdit={toggleTodoEdit}
-          editTodo={editTodo}
-          selectTodo= {selectTodo}
-        />
+          <AddToDo addTodo={addTodo} />
+          <TodoList
+            todoList={state.todoList}
+            deleteTodo={deleteTodo}
+            toggleTodo={toggleTodo}
+            toggleTodoEdit={toggleTodoEdit}
+            editTodo={editTodo}
+            selectTodo={selectTodo}
+          />
+        </div>
       </div>
-    </div>
     </themeContext.Provider>
   );
 }
